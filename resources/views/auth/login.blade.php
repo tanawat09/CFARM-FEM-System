7e4191a7580d406a3f5ad4667880cfc46d9cbd79
unknown <max091235@gmail.com>
2026-06-08 17:13:32 +0700

    Fix login CSRF behind Portainer host

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -132,7 +132,7 @@
                     </div>
                 @endif
 
-                <form method="POST" action="{{ route('login') }}">
+                <form method="POST" action="{{ route('login', [], false) }}">
                     @csrf
 
                     <!-- Email Address -->
